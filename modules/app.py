83e588b34b63f08d314ccb94f4b02efb0f6f4a51
sambapython@gmail.com <sambapython@gmail.com>
2019-01-08 09:19:13 +0530

    file as a module.

---
+++ modules/app.py
@@ -0,0 +1,11 @@
+#import sys
+#sys.path.append("/home/khyaathipython")
+#sys.path.insert(1,"/home/khyaathipython")
+#print(sys.path)
+'''
+import file1
+file1.fun()
+print(file1.a,file1.b,file1.c,file1.d)
+'''
+import file1
+file1.fun()
---
+++ modules/file1.py
@@ -0,0 +1,9 @@
+a=100
+b=200
+c=a+b
+d=a-b
+def fun():
+	print("this is fun in file1")
+print("this is file1")
+fun()
+print(a,b,c,d)
