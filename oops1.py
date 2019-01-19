9dec5a595992c673f5f100d77a5ea18755af16d5
sambapython@gmail.com <sambapython@gmail.com>
2019-01-19 10:58:02 +0530

    oops.

---
+++ oops1.py
@@ -0,0 +1,7 @@
+class Emp:
+    print("this is class")
+    company_name="KHYAATHI" # attribute
+    def sal_cal(a): # method or operation
+        print("this is fun in class Emp")
+print(Emp.company_name)
+Emp.sal_cal(20)
